9822631623f8b80704c7ea81d39c10d16aaabd30
Jeremy <jeremytee97@gmail.com>
2019-09-12 12:00:56 +0800

    Add facebook login details for user to replace with their values

---
+++ facebook_login_details.py
@@ -0,0 +1,6 @@
+# facebook login for your messenger bot
+sender_email = '<INSERT YOUR FACEBOOK EMAIL>'
+sender_password = '<INSERT YOUR FACEBOOK PASSWORD>'
+
+# facebook id for receiver
+receiver_id = '<INSERT RECEIVER ID>'
